38214fe19e82b82d203e03f50baa4be30260e0bd
Paul Tagliamonte <paul.tagliamonte@va.gov>
2016-02-22 15:17:58 -0500

    Add some more doc strings

--- lib/bgs/services/claimant.rb
+++ lib/bgs/services/claimant.rb
@@ -5,16 +5,23 @@
 # worldwide through the CC0 1.0 Universal public domain dedication.
 
 module BGS
+  # This service gets information about a claimant.
   class ClaimantWebService < BGS::Base
     def bean_name
       "ClaimantServiceBean"
     end
 
+    # findPOAByPtcntId (shrinqf)
+    #   finds the Power of Attorney related to a participant ID.
     def find_poa_by_participant_id(id)
       response = request(:find_poa_by_ptcpnt_id, "ptcpntId": id)
       response.body[:find_poa_by_ptcpnt_id_response][:return]
     end
 
+    # findGeneralInformationByPtcpntIds (shrinqm)
+    #   finds the General Information for given ptcpntIds, flashes, diaries,
+    #   and evrs. Used when a list exist, and you want information on a single
+    #   claimant
     def find_general_information_by_participant_id(id)
       response = request(:find_general_information_by_ptcpnt_id, "ptcpntId": id)
       response.body[:find_general_information_by_ptcpnt_id_response][:return]
