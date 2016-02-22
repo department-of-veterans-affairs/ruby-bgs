0d24f832c8e4bf7da5772e17ae66a0dd46b602a3
Paul Tagliamonte <paul.tagliamonte@va.gov>
2016-02-22 10:03:50 -0500

    Make the API more readable

--- lib/bgs/services/claimant.rb
+++ lib/bgs/services/claimant.rb
@@ -5,18 +5,17 @@
 # worldwide through the CC0 1.0 Universal public domain dedication.
 
 module BGS
-
   class ClaimantWebService < BGS::Base
     def bean_name
       "ClaimantServiceBean"
     end
 
-    def find_poa_by_ptcpnt_id(id)
+    def find_poa_by_participant_id(id)
       response = request(:find_poa_by_ptcpnt_id, {"ptcpntId": id})
       return response.body[:find_poa_by_ptcpnt_id_response][:return]
     end
 
-    def find_general_information_by_ptcpnt_id(id)
+    def find_general_information_by_participant_id(id)
       response = request(:find_general_information_by_ptcpnt_id, {"ptcpntId": id})
       return response.body[:find_general_information_by_ptcpnt_id_response][:return]
     end
