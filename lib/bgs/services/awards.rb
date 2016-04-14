e8d19f1d959ce853ca3728094f4d12d75aa8436e
Paul Tagliamonte <paul.tagliamonte@va.gov>
2016-04-14 17:30:35 -0400

    Cleanup style

--- lib/bgs/services/awards.rb
+++ lib/bgs/services/awards.rb
@@ -9,7 +9,6 @@ module BGS
   # interested in. This information may be kept permanently, removed or discarded
   # if appropriate.
   class AwardWebService < BGS::Base
-
     def bean_name
       "AwardWebServiceBean"
     end
@@ -18,23 +17,5 @@ module BGS
       response = request(:find_award_bene_by_ptcpnt_vet_id, "ptcpntVetId": participant_id)
       response.body[:findAwardBeneByPtcpntVetIdResponse]
     end
-
   end
 end
-
-# AwardWebService
-# Share Web Service
-# Service Description
-# This service is used to find the Award information.
-# Methods
-# findStationOfJurisdiction - find the current Station of Jurisdiction by Participant Veteran ID and Participant
-# Beneficiary ID
-# findAwardBene - find the Award Beneficiaries by Participant Veteran ID, Participant Beneficiary ID, Participant
-# Recipient ID and Award Type Code
-# findVeteranAwardCmpsitBySSN – find the Award Composite by SSN
-# findVeteranAwardCmpsitByFileNumber - find the Award Composite by File Number
-# findVeteranAwardCmpsitByPtcpntId – find the Award Composite by Participant ID
-# findAwardBeneByPtcpntVetId – find the Award Beneficiaries by Participant Veteran ID
-# findAwardBeneByPtcpntBeneId – find the Award Beneficiaries by Participant Beneficiary ID
-# findAwardBeneByPtcpntRecipId - find the Award Beneficiaries by Participant Recipient ID
-# findAwardBeneByFileNumber - find the Award Beneficiaries by File Number OR SSN and Participant Veteran ID
