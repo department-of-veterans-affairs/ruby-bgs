b3d32b4b15f9db749e424a3dca4860d03bac59db
Mark Benjamin <mdbenjam@gmail.com>
2018-07-19 17:00:05 -0400

    Changing the name

--- lib/bgs/services/security.rb
+++ lib/bgs/services/security.rb
@@ -5,7 +5,7 @@ module BGS
     end
 
     # Gets a users' participant id from their station and css ids
-    def find_ptcpnt_id(station_id:, css_id:)
+    def get_participant_id_for_user(station_id:, css_id:)
       response = request(:find_ptcpnt_id, "stationNumber": station_id, "userId": css_id)
       response.body[:find_ptcpnt_id_response][:return]
     end
