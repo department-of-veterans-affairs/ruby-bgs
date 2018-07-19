cacc3effa76fc3134c63e88c9e50157c333168a7
Mark Benjamin <mdbenjam@gmail.com>
2018-07-19 13:40:35 -0400

    Add security and org endpoints (#59)
    * Adding relationship endpoints
    
    * Adding new services
    
    * Naming parameters
    
    * Adding comments

---
+++ lib/bgs/services/security.rb
@@ -0,0 +1,13 @@
+module BGS
+  class SecurityWebService < BGS::Base
+    def self.service_name
+      "security"
+    end
+
+    # Gets a users' participant id from their station and css ids
+    def find_ptcpnt_id(station_id:, css_id:)
+      response = request(:find_ptcpnt_id, "stationNumber": station_id, "userId": css_id)
+      response.body[:find_ptcpnt_id_response][:return]
+    end
+  end
+end
