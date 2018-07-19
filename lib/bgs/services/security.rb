0447ac239b63e0698ef8b4ac0fefd2e16f80f01f
Mark Benjamin <mdbenjam@gmail.com>
2018-07-19 13:07:44 -0400

    Naming parameters

--- lib/bgs/services/security.rb
+++ lib/bgs/services/security.rb
@@ -4,8 +4,8 @@ module BGS
       "security"
     end
 
-    def find_ptcpnt_id(station_id, user)
-      response = request(:find_ptcpnt_id, "stationNumber": station_id, "userId": user)
+    def find_ptcpnt_id(station_id:, css_id:)
+      response = request(:find_ptcpnt_id, "stationNumber": station_id, "userId": css_id)
       response.body[:find_ptcpnt_id_response][:return]
     end
   end
