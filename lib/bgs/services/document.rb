1b0f2fde384c71897400f9f2e4c49bcb29a5876e
Sally Maki <sallymaki@gmail.com>
2018-09-25 17:20:30 -0700

    Fix Gyoku casing, return doc id only for manage claimant letter

--- lib/bgs/services/document.rb
+++ lib/bgs/services/document.rb
@@ -14,19 +14,21 @@ module BGS
     end
 
     def manage_claimant_letter_V2(letter)
+      # manage_claimant_letter__V2 converts to manageClaimantLetter_V2
       response = request(
-        :manage_claimant_letter_V2,
+        :manage_claimant_letter__V2,
         "letter": {
           "letters": letter
         },
         "generateClaimantLetter": true
       )
 
-      response.body[:manage_claimant_letter_V2_response][:return]
+      response.body[:manage_claimant_letter__V2][:return][:envelope][:letters][:doc_id]
     end
 
     # Generates the tracked items based on developmentActions in the letters
     # in the claimant letter
+    # Returns the BenefitClaim, which will include the development actions and letters
     def generate_tracked_items(claim_id)
       response = request(:generate_tracked_items, "claimId": claim_id)
       response.body[:generate_tracked_items_response][:return]
