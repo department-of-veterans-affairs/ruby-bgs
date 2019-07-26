da5802c35f47f152b767a4581f88e9ad4c5cf9aa
Peter Karman <peter@truss.works>
2019-07-26 14:20:28 -0500

    Add veteran.find_by_participant_id service

--- lib/bgs/services/veteran.rb
+++ lib/bgs/services/veteran.rb
@@ -15,10 +15,21 @@ module BGS
       "veteran"
     end
 
+    def find_by_participant_id(participant_id)
+      response = request(:find_veteran_by_ptcpnt_id, "ptcpntId": participant_id)
+      response = response.body[:find_veteran_by_ptcpnt_id_response][:return]
+      merge_birls_and_corp_records(response)
+    end
+
     def find_by_file_number(file_number)
       response = request(:find_veteran_by_file_number, "fileNumber": file_number)
       response = response.body[:find_veteran_by_file_number_response][:return]
+      merge_birls_and_corp_records(response)
+    end
 
+    private
+
+    def merge_birls_and_corp_records(response)
       birls_record = response[:vet_birls_record] || {}
       corp_record = response[:vet_corp_record] || {}
       backwards_compatibility = {
