b23c1877313eff83fcc2d5f50a5add9d303434c6
Peter Karman <peter@truss.works>
2020-01-21 10:40:24 -0600

    wip

---
+++ lib/bgs/services/common_security.rb
@@ -0,0 +1,20 @@
+module BGS
+  class CommonSecurityServiceImplWSV1 < BGS::Base
+    def self.service_name
+      "common_security"
+    end
+
+    def bean_name
+      "css-webservices"
+    end
+
+    def get_css_user_stations(username)
+      response = request(:get_css_user_stations_by_application_username, username)
+    end
+
+    def get_security_profile(username:, station_id:, application:)
+      # should all be in the header
+      response = request(:get_security_profile_from_context)
+    end
+  end
+end
